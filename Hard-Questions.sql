e6e6c62ce8d25723ebc55c1af7467314b2159546
Euphemiaxx <79640443+Euphemiaxx@users.noreply.github.com>
2023-04-26 13:24:29 +0800

    Create Hard-Questions.sql

---
+++ Hard-Questions.sql
@@ -0,0 +1,55 @@
+
+/* Airbnb - Given a table of rental property searches by users, 
+            find the minimum, average, maximum rental prices for each host’s popularity rating*/
+
+WITH rating_agg AS(
+    SELECT
+        host_pop_rating,
+        min(price) min_price,
+        avg(price) avg_price,
+        max(price) max_price,
+        CASE
+            WHEN host_pop_rating = 'New' THEN 1
+            WHEN host_pop_rating = 'Hot' THEN 2
+            WHEN host_pop_rating = 'Rising' THEN 4
+            WHEN host_pop_rating = 'Trending Up' THEN 5
+            WHEN host_pop_rating = 'Popular' THEN 3
+        END AS rating_order
+    FROM
+        (
+            SELECT
+                hosts_id,
+                price,
+                number_of_reviews,
+                CASE
+                    WHEN number_of_reviews = 0 THEN 'New'
+                    WHEN number_of_reviews BETWEEN 1 AND 5 THEN 'Rising'
+                    WHEN number_of_reviews BETWEEN 6 AND 15 THEN 'Trending Up'
+                    WHEN number_of_reviews BETWEEN 16 AND 40 THEN 'Popular'
+                    ELSE 'Hot'
+                END AS host_pop_rating
+            FROM
+                (
+                    SELECT
+                        concat(price, room_type, host_since, zipcode, number_of_reviews) hosts_id,
+                        max(price) price,
+                        max(number_of_reviews) number_of_reviews
+                    FROM
+                        airbnb_host_searches
+                    GROUP BY
+                        concat(price, room_type, host_since, zipcode, number_of_reviews)
+                ) AS hosts
+        ) AS hosts_rating
+    GROUP BY
+        host_pop_rating
+)
+
+SELECT
+    host_pop_rating,
+    min_price,
+    avg_price,
+    max_price
+FROM
+    rating_agg
+ORDER BY
+    rating_order ASC;
